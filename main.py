4a8a10e0264ff807cc9ecd48aeac585cb5b09bca
blasterizer <blasterizer007@gmail.com>
2023-08-15 20:30:28 +0530

    Added Links, Specified Language, Limit news, added subject and control category

--- main.py
+++ main.py
@@ -1,25 +1,24 @@
 import requests
 from send_email import send_email
 
+topic = "tesla"
+
 api_key = "1389a9f6a3ac44c1963f33babc19a3ca"
 
-url = 'https://newsapi.org/v2/everything?q=tesla&from=2023-07-14&' \
-      'sortBy=publishedAt&apiKey=1389a9f6a3ac44c1963f33babc19a3ca'
+url = (f"https://newsapi.org/v2/everything?q={topic}&from=2023-07-15&sortBy=publishedAt&apiKey"
+       "=1389a9f6a3ac44c1963f33babc19a3ca&language=en")
 
 request = requests.get(url)
 
 content = request.json()
 
-body = ""
-for article in content["articles"]:
-    if article["title"] is not None:
-        body = body + article["title"].bold() + "\n" + article["description"] + 2*"\n"
+body = "Subject: Today's News" + "\n"
 
-message = f"""\
-Subject: News Regarding Tesla 
+for article in content["articles"][:20]:
+    if (article["description"] is not None) & (article["title"] is not None):
+        body = body + article["title"] + "\n" \
+               + article["description"] \
+               + "\n" + article["url"] + 2*"\n"
 
-{body}
-"""
-message = message.encode("utf-8")
-send_email(message)
-
+body = body.encode("utf-8")
+send_email(body)

d3ed7c06a037e2665914c2b1b6c4c8f9b2e5d976
blasterizer <blasterizer007@gmail.com>
2023-08-17 22:27:16 +0530

    Added Tech Crunch News to the Daily News Digest

--- main.py
+++ main.py
@@ -1,14 +1,11 @@
 import requests
 from send_email import send_email
 
-topic = "tesla"
+topic = "techcrunch"
 
 api_key = "1389a9f6a3ac44c1963f33babc19a3ca"
 
-url = ("https://newsapi.org/v2/everything?"
-       f"q={topic}&"
-       "from=2023-07-16&sortBy=publishedAt&"
-       "apiKey=1389a9f6a3ac44c1963f33babc19a3ca")
+url = f"https://newsapi.org/v2/top-headlines?sources={topic}&apiKey=1389a9f6a3ac44c1963f33babc19a3ca"
 
 request = requests.get(url)
 
